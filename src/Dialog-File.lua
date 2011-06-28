2552e9954c980f5b5c4bf4b5f57aeb44c1b99eb5
speveril <speveril@gmail.com>
2011-06-27 23:44:13 -0700

    A slightly less hobo file dialog. You can scroll with the mouse and click things (but clicking just selects; have to hit enter to choose it for now)

--- src/Dialog-File.lua
+++ src/Dialog-File.lua
@@ -22,14 +22,18 @@ Dialog.File = function(which, title)
     
     local file = nil
     local done = false
-    local file_scroll = 1
+    local file_scroll = 0
     local num_lines = 1 + math.floor((h - f_h - 20) / (f_h + 3))
     local selected = 1
     local file_list = v3.ListFilePattern(Dialog.state.File[which].directory .. "/*.*"):Explode("|")
     if file_list[1] == '.' then table.remove(file_list, 1) end
     table.remove(file_list) -- clip off the blank one at the end
     
-    
+    -- fs = file scroll
+    local fs_x = x + 69
+    local fs_y = y + 3 + (f_h + 4)
+    local fs_w = w - 72
+    local fs_h = h - 13 - (fs_y - y)
 
     while true do
         v3.UpdateControls()
@@ -41,10 +45,10 @@ Dialog.File = function(which, title)
         f:Print(x + 4, y + 4 + (f_h + 4) * 0, "Folder:", vx.screen)
         f:Print(x + 70, y + 4 + (f_h + 4) * 0, Dialog.state.File[which].directory .. "/", vx.screen)
         
-        vx.screen:RectFill(x + 69, y + 3 + (f_h + 4) * 1, x + w - 3, y + h - 13, colors.white)
+        vx.screen:RectFill(fs_x, fs_y, fs_x + fs_w, fs_y + fs_h, colors.white)
         f:Print(x + 4, y + 4 + (f_h + 4) * 1, "Filename:", vx.screen)
                 
-        local line = file_scroll
+        local line = file_scroll + 1
         for i,v in ipairs(file_list) do
             if line > 0 and line <= num_lines then
                 if i == selected then
@@ -57,30 +61,47 @@ Dialog.File = function(which, title)
             line = line + 1
         end
         
+        assets.cursors.default:Blit(vx.mouse.x - 8, vx.mouse.y - 8)
+        
         if vx.key.Down.pressed then
             vx.key.Down:Delay(15)
             selected = selected + 1
             if selected > #file_list then selected = #file_list end
-            if selected + file_scroll - 1 > num_lines then file_scroll = file_scroll - 1 end
+            if selected + file_scroll > num_lines then file_scroll = file_scroll end
         end
         if vx.key.Up.pressed then
             vx.key.Up:Delay(15)
             selected = selected - 1
             if selected < 1 then selected = 1 end
-            if selected + file_scroll - 1 <= 0 then file_scroll = file_scroll + 1 end
+            if selected + file_scroll <= 0 then file_scroll = file_scroll end
         end
         if vx.key.PageDown.pressed then
             vx.key.PageDown:Delay(15)
             selected = selected + math.floor(num_lines / 2)
             if selected > #file_list then selected = #file_list end
-            while selected + file_scroll - 1 > num_lines do file_scroll = file_scroll - 1 end
+            while selected + file_scroll > num_lines do file_scroll = file_scroll end
         end
         if vx.key.PageUp.pressed then
             vx.key.PageUp:Delay(15)
             selected = selected - math.floor(num_lines / 2)
             if selected < 1 then selected = 1 end
-            while selected + file_scroll - 1 <= 0 do file_scroll = file_scroll + 1 end
+            while selected + file_scroll <= 0 do file_scroll = file_scroll end
         end
+        
+        local m_wheel_d = vx.mouse:SampleWheelDelta()
+        if m_wheel_d ~= 0 then
+            file_scroll = file_scroll + m_wheel_d
+            if file_scroll > 0 then file_scroll = 0 end
+            if -file_scroll > #file_list - num_lines then file_scroll = -(#file_list - num_lines) end
+            if selected + file_scroll < 1 then selected = -file_scroll + 1 end
+            if selected + file_scroll > num_lines then selected = -file_scroll + num_lines end
+        end
+        
+        if UI.mouseIsIn(x + 69, y + 3 + (f_h + 4) * 1, x + w - 3, y + h - 13) and vx.mouse.left.pressed then
+            local s = math.floor((vx.mouse.y - (y + 3 + (f_h + 3) * 1)) / (f_h + 3)) - file_scroll + 1
+            selected = s
+        end
+        
         if vx.key.Enter.pressed then
             vx.key.Enter:Unpress()
             if file_list[selected] == ".." then
@@ -90,7 +111,7 @@ Dialog.File = function(which, title)
             end
             
             selected = 1
-            file_scroll = 1
+            file_scroll = 0
             
             file_list = v3.ListFilePattern(Dialog.state.File[which].directory .. "/*.*"):Explode("|")
             if #file_list == 1 then
--- src/lib/vx/_vergeMouseInput.lua
+++ src/lib/vx/_vergeMouseInput.lua
@@ -3,6 +3,7 @@ vergeclass "_vergeMouseInput"
 		self.left = vx._vergeMouseButtonInput("l")
 		self.right = vx._vergeMouseButtonInput("r")
 		self.middle = vx._vergeMouseButtonInput("m")
+                self.last_wheel_sample = nil
 	end
 	
 	_vergeMouseInput._property('x', function(self) return v3.mouse.x end)
@@ -12,6 +13,17 @@ vergeclass "_vergeMouseInput"
 	function _vergeMouseInput:__tostring()
 		return "verge mouse input " .. ObjectAttributesToString(self)
 	end
+        
+        function _vergeMouseInput:SampleWheelDelta()
+            if self.last_wheel_sample then
+                local d = self.wheel - self.last_wheel_sample
+                self.last_wheel_sample = self.wheel
+                return d/120
+            else
+                self.last_wheel_sample = self.wheel
+                return 0
+            end
+        end
 
 vergeclass "_vergeMouseButtonInput"
 	function _vergeMouseButtonInput:__init(button)
@@ -25,7 +37,7 @@ vergeclass "_vergeMouseButtonInput"
 	function _vergeMouseButtonInput:Unpress()
 		self.pressed = false
 	end
-	
+        
 	function _vergeMouseButtonInput:__tostring()
 		return "verge mouse button " .. ObjectAttributesToString(self)
 	end
